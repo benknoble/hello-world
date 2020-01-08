2f11c80aee94949b1a9f439856e24cdbf0a7c9cd
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-01-08 15:23:45 -0500

    520 update

--- code/Beta.java
+++ code/Beta.java
@@ -1,6 +1,10 @@
 class Beta {
-    public Beta b;
-    public void test(int x) {
-        Beta c = this.b;
-    }
+  public Beta b;
+  public void test(int x) {
+    // int b = 1; // OK
+    // Beta x = null; // BAD
+    // int Beta = 1; // OK
+    // Beta b = b; // BAD
+    // Beta b = this.b; // OK
+  }
 }
