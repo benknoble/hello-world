5efb9f07bef23dd114ab6b33286efc36e9ade009
D. Ben Knoble <ben.knoble+github@gmail.com>
2024-05-10 18:56:04 -0400

    fix deps

--- code/laci/info.rkt
+++ code/laci/info.rkt
@@ -4,3 +4,4 @@
 (define pkg-desc "Based on https://cs.uwaterloo.ca/~plragde/flaneries/LACI/index.html")
 (define version "0.0")
 (define pkg-authors '(benknoble))
+(define build-deps '("rackunit-lib"))
